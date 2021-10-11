049fe3ffa5ac8873e93fb9b678a5e23e70bc24dc
Kryštof Regent <1009@student.itgymnazium.cz>
2021-10-11 18:53:34 +0200

    loader---getFilelist

--- lib/DL/loader/loader.dart
+++ lib/DL/loader/loader.dart
@@ -1,8 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 
+late String contentServiceUrl;
+
+Future getContentServiceUrl() async {
+  contentServiceUrl = await loadAssetString('contentServiceUrl');
+}
+
 Future<String> getSheet() async {
-  String contentServiceUrl = await loadAssetString('contentServiceUrl');
   String fileId = await loadAssetString('fileId');
   String sheetName = await loadAssetString('sheetName');
 
@@ -16,5 +21,27 @@ Future<String> getSheet() async {
 }
 
 Future<String> loadAssetString(String varname) async {
-  return await rootBundle.loadString('config/$varname.txt');
+  try {
+    return await rootBundle.loadString('config/$varname.txt');
+  } catch (_) {
+    return '';
+  }
+}
+
+Future<String> getFilelist() async {
+  String contentServiceUrl = await loadAssetString('contentServiceUrl');
+
+  String fileId = '1LZlPCCI0TwWutwquZbC8HogIhqNvxqz0AVR1wrgPlis';
+  //await loadAssetString('fileId');
+
+  String sheetName = 'fileListProHledace';
+  // await loadAssetString('sheetName');
+  try {
+    var response = await Dio()
+        .get(contentServiceUrl + '?filelistid=$fileId&sheetname=$sheetName');
+    print(response.data);
+    return response.data;
+  } catch (e) {
+    return '';
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sheetviewer/DL/loader/loader.dart';
 
 import 'AL/views/grid_view/_grid_view_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await getContentServiceUrl();
+  await getFilelist();
   runApp(const GridViewApp());
 }
