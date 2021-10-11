37e2fed30f474d25415da2c6da278a22dd99be87
Kryštof Regent <1009@student.itgymnazium.cz>
2021-10-11 16:58:35 +0200

    2nd---filelist1

--- bakzip.bat
+++ bakzip.bat
@@ -10,11 +10,9 @@ del *.7z
 C:\uti\7-Zip\7z.exe a %1%-%%MYDATE%  *.rules fire*.* depl*.* lib\* test\* web\*.* web\help\*.* docs\* web\appsScript\*.*  pubspec.yaml *.bat *.ps1  -r
 
 xcopy *.7z  "c:\dev\zBak\SheetViewer\" /C /Y
-xcopy *.7z  "C:\Users\janre\OneDrive\SheetViewer\" /C /S /M  /Y
 xcopy *.7z  "C:\Users\janre\Dropbox\Drop2NAS\" /C /Y
 
-xcopy *.7z  "p:\pCloud"  /C /Y
-xcopy *.7z  "g:\Můj disk\z.rest\_dev\SheetViewer\"  /C /Y
+xcopy *.7z  "p:\pCloud\"  /C /Y
 xcopy *.7z  "C:\Asus WebStorage\jan.regent@gmail.com\MySyncFolder\backup\SheetViewer" /C /Y
 pause
 call powershell .\bakftp.ps1
