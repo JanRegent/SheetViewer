7907506bc424c64b87abdd60a537911fb5e54a71
Kryštof Regent <1009@student.itgymnazium.cz>
2021-10-13 09:47:44 +0200

    build---testApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:sheetviewer/DL/loader/loader.dart';
 
-import 'AL/filelistgrid.dart';
+//import 'AL/filelistgrid.dart';
+import './counter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await getContentServiceUrl();
 
-  runApp(const FilelistGridApp());
+  //runApp(const FilelistGridApp());
+  runApp(const CounterApp());
 }
