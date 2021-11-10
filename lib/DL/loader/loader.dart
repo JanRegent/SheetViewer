0b7851d5cce94936cd5eecb3eef30f6bcecf63ef
Kryštof Regent <1009@student.itgymnazium.cz>
2021-11-10 11:04:45 +0100

    DL-loader---cache-filelist

--- lib/DL/loader/loader.dart
+++ lib/DL/loader/loader.dart
@@ -1,5 +1,6 @@
+import 'package:cache_manager/core/delete_cache_service.dart';
 import 'package:cache_manager/core/read_cache_service.dart';
-import 'package:cache_manager/core/write_cache_service.dart';
+import 'package:cache_manager/core/write_cache_service.dart'; //https://pub.dev/packages/cache_manager
 import 'package:dio/dio.dart';
 
 import 'package:flutter/services.dart';
@@ -13,36 +14,14 @@ Future getContentServiceUrl() async {
 Future<String> sheetGet(String fileId, String sheetName) async {
   String key = 'fileid=$fileId&sheetname=$sheetName';
 
-  String jsonString = await sheetLoad(key);
+  String jsonString = await readString(key);
   if (jsonString.isNotEmpty) return jsonString;
 
   try {
     var response = await Dio().get(contentServiceUrl + '?' + key);
-    sheetSave(key, response.data);
+    updateString(key, response.data);
     return response.data;
   } catch (e) {
-    return '';
-  }
-}
-
-Future sheetLoad(String key) async {
-  try {
-    String jsonString = await ReadCache.getString(key: key);
-    // ignore: unnecessary_null_comparison
-    if (jsonString == null) return '';
-    return jsonString;
-  } catch (e) {
-    //rint(e); //Do something if error occurs
-    return '';
-  }
-}
-
-Future sheetSave(String key, String jsonString) async {
-  try {
-    await WriteCache.setString(key: key, value: jsonString);
-    return 'OK';
-  } catch (e) {
-    //rint(e); //Do something if error occurs
     return '';
   }
 }
@@ -64,9 +43,47 @@ Future<String> getFilelist() async {
   String sheetName = 'fileListProHledace';
   // await loadAssetString('sheetName');
   try {
-    var response = await Dio()
-        .get(contentServiceUrl + '?filelistid=$fileId&sheetname=$sheetName');
+    String key = 'filelistid=$fileId&sheetname=$sheetName';
+
+    String jsonString = await readString(key);
+    if (jsonString.isNotEmpty) return jsonString;
+
+    var response = await Dio().get(contentServiceUrl + '?' + key);
+    updateString(key, response.data);
     return response.data;
+  } catch (e) {
+    return '';
+  }
+}
+
+//-------------------------------------------------------------------CRUD
+
+Future updateString(String key, String jsonString) async {
+  try {
+    await WriteCache.setString(key: key, value: jsonString);
+    return 'OK';
+  } catch (e) {
+    //rint(e); //Do something if error occurs
+    return '';
+  }
+}
+
+Future readString(String key) async {
+  try {
+    String jsonString = await ReadCache.getString(key: key);
+    // ignore: unnecessary_null_comparison
+    if (jsonString == null) return '';
+    return jsonString;
+  } catch (e) {
+    //rint(e); //Do something if error occurs
+    return '';
+  }
+}
+
+Future deleteString(String key) async {
+  try {
+    await DeleteCache.deleteKey(key, key = key);
+    return 'OK';
   } catch (e) {
     return '';
   }
