6d9ac0972655fb206f73171fe698b4164e10dbce
Kryštof Regent <1009@student.itgymnazium.cz>
2021-12-11 20:17:31 +0100

    index-html--canvaskit-render---

--- lib/uti/viewers/json_viewer.dart
+++ lib/uti/viewers/json_viewer.dart
@@ -6,8 +6,6 @@ class JsonViewerPage extends StatelessWidget {
   final testObj;
   const JsonViewerPage(this.testObj, {Key? key}) : super(key: key);
 
-  //final testObj = json.decode(testStringObj);
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -28,114 +26,7 @@ class JsonViewerPage extends StatelessWidget {
               // in the middle of the parent.
               children: [
                 JsonViewer(testObj),
-                const Text('Array example'),
-                JsonViewer(testArray)
               ]),
         ));
   }
 }
-
-const testStringObj1 = '''{
-      "ParaString": "www.apigj.com",
-      "ParaObject": {
-        "ObjectType": "Api",
-        "ObjectName": "Manager",
-        "ObjectId": "Code",
-        "FatherId": "Generator"
-      },
-      "ParaLong": 6222123188092928,
-      "ParaInt": 5303,
-      "ParaFloat": -268311581425.664,
-      "ParaBool": false,
-      "ParaArrString": [
-        "easy",
-        "fast"
-      ],
-      "ParaArrObj": [
-        {
-          "SParaString": "Work efficiently long words long words long words long words long words long words long words long words long words long words long words long words long words ",
-          "SParaLong": 7996655703949312,
-          "SParaInt": 8429,
-          "SParaFloat": -67669103057.3056,
-          "SParaBool": false,
-          "SParaArrString": [
-            "har",
-            "zezbehseh"
-          ],
-          "SParaArrLong": [
-            6141464276893696,
-            2096646955466752
-          ],
-          "SParaArrInt": [
-            1601,
-            757
-          ],
-          "SParaArrFloat": [
-            -643739466439.0656,
-            -582978647149.7728
-          ],
-          "SParaArrBool": [
-            false,
-            false
-          ]
-        },
-        {
-          "SParaString": "Let's go",
-          "SParaLong": 641970970034176,
-          "SParaInt": 37,
-          "SParaFloat": 556457726574.592,
-          "SParaBool": false,
-          "SParaArrString": [
-            "miw",
-            "aweler"
-          ],
-          "SParaArrLong": [
-            3828767638159360,
-            7205915801419776
-          ],
-          "SParaArrInt": [
-            1187,
-            6397
-          ],
-          "SParaArrFloat": [
-            -744659811617.9968,
-            494621489417.4208
-          ],
-          "SParaArrBool": [
-            true,
-            false
-          ]
-        }
-      ],
-      "ParaArrLong": [
-        7607846344589312,
-        7840335854043136
-      ],
-      "ParaArrInt": [
-        2467,
-        1733
-      ],
-      "ParaArrFloat": [
-        759502472845.7216,
-        -157877664743.424
-      ],
-      "ParaArrBool": [
-        true,
-        true
-      ]
-    }''';
-
-const testArray = [
-  {
-    "id": 1,
-    "array": [
-      {'nested': true}
-    ],
-  },
-  {
-    "id": 2,
-    "array": [
-      {'nested': false}
-    ],
-  }
-];
