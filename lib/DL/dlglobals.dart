94684be30d69c011b6927d44e837e463ea7a92f9
Kryštof Regent <1009@student.itgymnazium.cz>
2022-05-15 17:27:45 +0200

    DL-buildVersion---

--- lib/DL/dlglobals.dart
+++ lib/DL/dlglobals.dart
@@ -9,18 +9,18 @@ class DlGlobals {
   Future init() async {
     baseUrl = await loadAssetString('contentServiceUrl');
     await appHome.updateString('DL-contentServiceUrl', baseUrl);
+
+    String buildVersion = await loadAssetString('buildVersion');
+    print(buildVersion);
+    await appHome.updateString('buildVersion', buildVersion);
   }
 }
 
 //-------------------------------------------------------------assets
 Future<String> loadAssetString(String varname) async {
-  //final file = XFile('config/$varname.txt');
-  //DateTime lm = await file.lastModified();
-  //contentServiceUrlLastModified = lm.toIso8601String();
-
   try {
     return await rootBundle.loadString('config/$varname.txt');
-  } catch (_) {
-    return '';
+  } catch (e) {
+    return e.toString();
   }
 }
