adc1645c04b6bd58ab67cea10ca79dd649d6e5dd
Kryštof Regent <1009@student.itgymnazium.cz>
2022-05-15 17:29:04 +0200

    DL-buildVersion-2---

--- bak.bat
+++ bak.bat
@@ -1,7 +1,7 @@
 
 call backendPull.bat
 $PSDefaultParameterValues['Out-File:Encoding'] = 'ascii'  
-'"%1 %2"' > Out-File -FilePath assets\config\buildVersion.txt
+"\"%1 %2\"" > Out-File -FilePath assets\config\buildVersion.txt
 call bakgit.bat %1 %2
 call bakzip.bat %1 %2
 
