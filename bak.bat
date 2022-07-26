9e570cb06819c54acd5205a7a98dfbd5bca5563b
Kryštof Regent <1009@student.itgymnazium.cz>
2022-07-26 15:54:19 +0200

    vercel-4e---

--- bak.bat
+++ bak.bat
@@ -5,7 +5,7 @@ SET YY=%DATE:~-4%
 SET MM=%DATE:~-7,2%
 SET DD=%DATE:~-10,2%
 ECHO In format DD/MM/YYYY - %DD%-%MM%-%YY%
-set SUBFILENAME='%1---%DD%-%MM%-%YY%'
+set SUBFILENAME='%1---%DD%-%MM%-%YY% %TIME%'
 echo const buildVersion = %SUBFILENAME%;     > lib\BL\appConfig\buildversion.dart 
 echo const appVersion = '22.08';    > lib\BL\appConfig\appversion.dart 
 call bakgit.bat %1 %2
--- lib/BL/appConfig/buildversion.dart
+++ lib/BL/appConfig/buildversion.dart
@@ -1,1 +1,1 @@
-const buildVersion = 'vercel-4d---26-07-2022';      
+const buildVersion = 'vercel-4e---26-07-2022 15:54:17,40';      
